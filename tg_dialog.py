21aa488bf95268282aee6a52871d1e2921bdebd1
COE516 <50444239+COE516@users.noreply.github.com>
2020-04-28 22:12:29 +0900

    update

--- tg_dialog.py
+++ tg_dialog.py
@@ -1,4 +1,4 @@
-# -- encoding utf-8 --
+# -*- encoding: utf-8 -*-
 
 import requests
 import json
@@ -8,7 +8,7 @@ import telegram
 
 #-----------------------------------------------------------------------------------------------------------------------------------------
 #커스텀 키보드 레이아웃을 생성(변경 및 추가 가능)
-custom_keyboard0 = [['help','btn1'],
+custom_keyboard0 = [['/help','btn1'],
                     ['btn2', 'btn3']]       #2 by 2 키보드 레이아웃
 #custom_keyboard1 = []
 
@@ -17,35 +17,35 @@ reply_markup0 = telegram.ReplyKeyboardMarkup(custom_keyboard0, resize_keyboard=T
 #-----------------------------------------------------------------------------------------------------------------------------------------
 
 
-def help_command(bot, update)
-    update.message.reply_text(문의사항은 010--로 연락주세요!, reply_markup = reply_markup0)
+def help_command(bot, update):
+    update.message.reply_text("문의사항은 010-****-****로 연락주세요!", reply_markup = reply_markup0)
     #이와 같은 방법으로 사용자에게 답변을 하면서 커스텀 키보드를 팝업시킬수있다.
 
     
 #--------------------------!!주석 부분의 api key 입력 부분 말고는 수정 x----------------------------------------------------------------------
 # dialog flow로 문자열을 보내서 답변을 반환하는 함수 
-def get_answer(text, user_key)
+def get_answer(text, user_key):
     data_send = { 
         
-        'query' text,
+        'query': text,
         
-        'sessionId' user_key,
+        'sessionId': user_key,
         
-        'lang' 'ko',
+        'lang': 'ko',
     }
     data_header = {
         
-        'Authorization' 'Bearer +본인의 dialogflow api+', #ed47255b~cb87 부분에 본인의 dialog flow api key를 입력(Bearer띄어쓰기유의)
+        'Authorization': 'Bearer +본인의 클라이언트 토큰 입력+', #ed47255b~cb87 부분에 본인의 dialog flow api key를 입력(Bearer띄어쓰기유의)
         
-        'Content-Type' 'applicationjson; charset=utf-8'
+        'Content-Type': 'application/json; charset=utf-8'
     }
     
-    dialogflow_url = 'httpsapi.dialogflow.comv1queryv=20150910'   
+    dialogflow_url = 'https://api.dialogflow.com/v1/query?v=20150910'   
     res = requests.post(dialogflow_url, data=json.dumps(data_send), headers=data_header)
     print(data_send)
     print(res.status_code, requests.codes.ok)
     
-    if res.status_code != requests.codes.ok
+    if res.status_code != requests.codes.ok:
         return '오류가 발생했습니다.'
     
     data_receive = res.json()
@@ -56,13 +56,13 @@ def get_answer(text, user_key)
 
 #---------------!!수정 x---------------------------------------------------------------------------
 #사용자의 텔레그램 메시지를 매개변수로 위의 get_answer()를 호출하여 돌아온 답변을 채팅방에 보내는 함수
-def get_message(bot, update)
+def get_message(bot, update):
     print(update.message.text)
     content = update.message.text
     print(content)
     chat_id = update.message.chat_id
     dialog_answer = get_answer(content, str(chat_id)) #사용자의 메시지에 대한 dialogflow의 답변이 리턴
-    update.message.reply_text(%s % dialog_answer) #챗봇이 사용자에게 답변
+    update.message.reply_text("%s" % dialog_answer) #챗봇이 사용자에게 답변
 #--------------------------------------------------------------------------------------------------
 
 
@@ -71,8 +71,8 @@ def get_message(bot, update)
 #풀링 방식으로 일정시간마다 다음을 반복
 #챗봇이 채팅방을 확인하며 새로운 메시지의 유무를 판단 새로운 메시지가 있으면 해당 메시지로 위의 get_message()를 호출
 #1. 원하는 기능의 핸들러를 추가(코드가 이해 되는 사람만.)
-def main()
-    updater = Updater('+본인이 생성한 텔레그램 봇 토큰 입력+') #본인이 생성한 텔레그램 봇 토큰 입력
+def main():
+    updater = Updater('+본인의 텔레그램 토큰 입력+') #본인이 생성한 텔레그램 봇 토큰 입력
     message_handler = MessageHandler(Filters.text, get_message)
     help_handler = CommandHandler('help', help_command)
     updater.dispatcher.add_handler(help_handler)
@@ -86,11 +86,11 @@ def main()
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
-@app.route('', methods=['POST', 'GET'])
-def index()
-    return 'h1welcome to chatbot worldh'
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return '<h1>welcome to chatbot world</h>'
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     main()
     app.run(host='0.0.0.0')
 #--------------------------------------------
